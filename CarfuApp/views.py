35bf1b6727a2fdbf6a02db29659e0f1b78a9c043
Sebastian <smutuku@kcbgroup.com>
2021-05-13 16:21:35 +0300

    Addition of Alter orders

--- CarfuApp/views.py
+++ CarfuApp/views.py
@@ -90,7 +90,7 @@ class Order(APIView):
     def get(self, request):
         model = Orders.objects.all()
         serializer = OrderSerializer(model, many=True)
-        return Response({"Message": "Success", "responsePayload": serializer.data})
+        return Response({serializer.data})
 
     def update():
         pass
