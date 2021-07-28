1aead688a992f92246859a622e33efbdff444fb3
Sebastian <smutuku@kcbgroup.com>
2021-07-28 23:06:33 +0300

    collect static

--- CarfuelBackEnd/settings.py
+++ CarfuelBackEnd/settings.py
@@ -176,12 +176,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
