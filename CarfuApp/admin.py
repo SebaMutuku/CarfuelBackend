4d316c3f099e21097621678d5dd26dedf2c99418
SebaMutuku <sebastian.muthiani@gmail.com>
2021-09-08 15:15:29 +0300

    Registration of Models

--- CarfuApp/admin.py
+++ CarfuApp/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from django.apps import apps
-models=apps.get_models()
-for model in models:
-    admin.register(model)
+from .models import Roles, Users, Orders, Registeredvehicles
+
+
+@admin.register(Roles)
+class RoleAdmin(admin.ModelAdmin):
+	list_display = [f.name for f in Roles._meta.fields]
+
+
+@admin.register(Users)
+class UserAdmin(admin.ModelAdmin):
+	list_display = [f.name for f in Users._meta.fields]
+
+
+@admin.register(Orders)
+class OrderAdmin(admin.ModelAdmin):
+	list_display = [f.name for f in Orders._meta.fields]
+
+
+@admin.register(Registeredvehicles)
+class RegisteredVehicleAdmin(admin.ModelAdmin):
+	list_display = [f.name for f in Registeredvehicles._meta.fields]
--- CarfuApp/serializers/OrderSerializer.py
+++ CarfuApp/serializers/OrderSerializer.py
@@ -7,47 +7,46 @@ from CarfuApp.models import Orders, Users
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = (
-            'orderid',
-            'ordernumber',
-            'customerid',
-            'orderamount',
-            'orderlocation',
-            'deliverytime',
-            'orderdetails',
-            'orderstatus',
-            'deliveryagent'
-        )
-        model = Orders
-
-    def createOrder(self, data):
-        ordernumber = data['ordernumber']
-        ordertime = datetime.datetime.now()
-
-        orderamount = data['orderamount']
-        orderlocation = data['orderlocation']
-        deliverytime = data['deliverytime']
-        orderdetails = data['orderdetails']
-        orderstatus = data['orderstatus']
-        try:
-            deliveryagent = Users.objects.get(user_id=data['deliveryagent'])
-            customerid = Users.objects.get(user_id=data['customerid'])
-        except Users.DoesNotExist:
-            return Serializer.error()
-
-
-        order = Orders.objects.create(orderstatus=orderstatus,
-                                      orderdetails=orderdetails,
-                                      ordertime=ordertime, orderamount=orderamount,
-                                      orderlocation=orderlocation, customerid=customerid,
-                                      deliverytime=datetime.datetime.now(), ordernumber=ordernumber,
-                                      deliveryagent=deliveryagent.username)
-        order.save()
-        print("Saved successfully ")
-        return order
-
-    def getAllOrders(self):
-        order = Orders.objects.all()
-        data = OrderSerializer(order, many=True)
-        return data
+	class Meta:
+		fields = (
+			'orderid',
+			'ordernumber',
+			'customerid',
+			'orderamount',
+			'orderlocation',
+			'deliverytime',
+			'orderdetails',
+			'orderstatus',
+			'deliveryagent'
+		)
+		model = Orders
+	
+	def createOrder(self, data):
+		ordernumber = data['ordernumber']
+		ordertime = datetime.datetime.now()
+		
+		orderamount = data['orderamount']
+		orderlocation = data['orderlocation']
+		deliverytime = data['deliverytime']
+		orderdetails = data['orderdetails']
+		orderstatus = data['orderstatus']
+		try:
+			deliveryagent = Users.objects.get(user_id=data['deliveryagent'])
+			customerid = Users.objects.get(user_id=data['customerid'])
+		except Users.DoesNotExist:
+			return Serializer.error()
+		
+		order = Orders.objects.create(orderstatus=orderstatus,
+		                              orderdetails=orderdetails,
+		                              ordertime=ordertime, orderamount=orderamount,
+		                              orderlocation=orderlocation, customerid=customerid,
+		                              deliverytime=datetime.datetime.now(), ordernumber=ordernumber,
+		                              deliveryagent=deliveryagent.username)
+		order.save()
+		print("Saved successfully ")
+		return order
+	
+	def getAllOrders(self):
+		order = Orders.objects.all()
+		data = OrderSerializer(order, many=True)
+		return data
