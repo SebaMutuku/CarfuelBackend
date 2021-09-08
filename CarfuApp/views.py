cf3c7571b7078e7de623de2a043e7a1acfa53a0e
SebaMutuku <sebastian.muthiani@gmail.com>
2021-09-08 21:28:05 +0300

    is_agent 2

--- CarfuApp/views.py
+++ CarfuApp/views.py
@@ -21,106 +21,107 @@ from .models import Users
 
 
 class Register(views.APIView):
-    permission_classes = (AllowAny,)
-    querySet = models.Users.objects.all()
-    serializer_class = RegisterSerializer
-    parser_classes(JSONParser, )
-    pagination_class = PageNumberPagination
-
-    def post(self, request):
-        print("Received Request  ",request)
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            user = serializer.addUser(request.data)
-            if user:
-                return Response({"User": serializer.data, "Message": "Successfully created user ['{}']".format(
-                    request.data.get('username'))},
-                                status=status.HTTP_200_OK)
-            else:
-                return Response({serializer.error},
-                                status=status.HTTP_401_UNAUTHORIZED)
-
-        else:
-            print(serializer.errors)
-            return Response("Invalid Credentials ", status.HTTP_401_UNAUTHORIZED)
-    def get(self,request):
-        model = Users.objects.all()
-        # serializer = RegisterSerializer(model, many=True)
-        if model is not None:
-            for user in model:
-                response={
-                "user_id":user.user_id,
-                "username":user.username,
-                "phonenumber":user.phonenumber,
-                "is_admin":user.is_admin,
-                "is_staff":user.is_agent
-                }
-                return Response(response,status=status.HTTP_200_OK)
-        else:
-            response = {"message": "Data not found"}
-            return Response(response, status=status.HTTP_200_OK)
-            
-
-
+	permission_classes = (AllowAny,)
+	querySet = models.Users.objects.all()
+	serializer_class = RegisterSerializer
+	parser_classes(JSONParser, )
+	pagination_class = PageNumberPagination
+	
+	def post(self, request):
+		print("Received Request  ", request)
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.addUser(request.data)
+			if user:
+				return Response({"User": serializer.data, "Message": "Successfully created user ['{}']".format(
+					request.data.get('username'))},
+				                status=status.HTTP_200_OK)
+			else:
+				return Response({serializer.error},
+				                status=status.HTTP_401_UNAUTHORIZED)
+		
+		else:
+			print(serializer.errors)
+			return Response("Invalid Credentials ", status.HTTP_401_UNAUTHORIZED)
+	
+	def get(self, request):
+		model = Users.objects.all()
+		# serializer = RegisterSerializer(model, many=True)
+		if model is not None:
+			for user in model:
+				response = {
+					"user_id": user.user_id,
+					"username": user.username,
+					"phonenumber": user.phonenumber,
+					"is_admin": user.is_admin,
+					"is_staff": user.is_agent,
+					"created_on": user.created_on,
+					"last_login": user.last_login,
+					"RoleName": user.roleid,
+					"Is_Active": user.is_active
+				}
+				return Response({"ResponsePayload": response}, status=status.HTTP_200_OK)
+		else:
+			response = {"message": "Data not found"}
+			return Response({"ResponsePayload": response}, status=status.HTTP_200_OK)
 
 
 class Login(APIView):
-    querySet = models.Roles.objects.all()
-    renderer_classes = (JSONRenderer,)
-    serializer_class = LoginSerializer
-    permission_classes = (AllowAny,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    parser_classes(JSONParser, )
-
-    def post(self, request):
-        print("Received Request  ",request)
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if serializer.is_valid():
-            token = serializer.authenticateUser(request.data)
-            if token:
-                response = {"message": "Successfully Logged In ", "token": token}
-                print("Response to API ", response)
-                return Response(response, status=status.HTTP_200_OK)
-            else:
-                response = {"message": "invalid Login Credentials", "token": token}
-                print("Response to API ", response)
-                return Response(response, status=status.HTTP_401_UNAUTHORIZED)
-        else:
-            response = {"message":"An Error ocurred", "token": token}
-            print("Response to API ",response)
-            return Response(response, status=status.HTTP_401_UNAUTHORIZED)
-    
+	querySet = models.Roles.objects.all()
+	renderer_classes = (JSONRenderer,)
+	serializer_class = LoginSerializer
+	permission_classes = (AllowAny,)
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	parser_classes(JSONParser, )
+	
+	def post(self, request):
+		print("Received Request  ", request)
+		serializer = self.serializer_class(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		if serializer.is_valid():
+			token = serializer.authenticateUser(request.data)
+			if token:
+				response = {"message": "Successfully Logged In ", "token": token}
+				print("Response to API ", response)
+				return Response(response, status=status.HTTP_200_OK)
+			else:
+				response = {"message": "invalid Login Credentials", "token": token}
+				print("Response to API ", response)
+				return Response(response, status=status.HTTP_401_UNAUTHORIZED)
+		else:
+			response = {"message": "An Error ocurred", "token": token}
+			print("Response to API ", response)
+			return Response(response, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class Order(APIView):
-    querySet = models.Orders.objects.all()
-    renderer_classes = (JSONRenderer,)
-    serializer_class = OrderSerializer
-    permission_classes = (AllowAny,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    parser_classes(JSONParser, )
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if serializer.is_valid():
-            order = serializer.createOrder(request.data)
-            if order:
-                response = {"message": "Order Successful", "OrderDetails": serializer.data}
-                return Response(response, status=status.HTTP_200_OK)
-            else:
-                response = {"message": "Order Failed", "OrderDetails": None}
-                return Response(response, status=status.HTTP_417_EXPECTATION_FAILED)
-
-        else:
-            response = {"message": "Failed", "OrderDetails": None}
-            return Response(response, status=status.HTTP_200_OK)
-
-    def get(self, request):
-        model = Orders.objects.all()
-        serializer = OrderSerializer(model, many=True)
-        return Response({"message": "Success", "responsePayload": serializer.data})
-
-    def update():
-        pass
+	querySet = models.Orders.objects.all()
+	renderer_classes = (JSONRenderer,)
+	serializer_class = OrderSerializer
+	permission_classes = (AllowAny,)
+	authentication_classes = (SessionAuthentication, BasicAuthentication)
+	parser_classes(JSONParser, )
+	
+	def post(self, request):
+		serializer = self.serializer_class(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		if serializer.is_valid():
+			order = serializer.createOrder(request.data)
+			if order:
+				response = {"message": "Order Successful", "OrderDetails": serializer.data}
+				return Response(response, status=status.HTTP_200_OK)
+			else:
+				response = {"message": "Order Failed", "OrderDetails": None}
+				return Response(response, status=status.HTTP_417_EXPECTATION_FAILED)
+		
+		else:
+			response = {"message": "Failed", "OrderDetails": None}
+			return Response(response, status=status.HTTP_200_OK)
+	
+	def get(self, request):
+		model = Orders.objects.all()
+		serializer = OrderSerializer(model, many=True)
+		return Response({"message": "Success", "responsePayload": serializer.data})
+	
+	def update():
+		pass
