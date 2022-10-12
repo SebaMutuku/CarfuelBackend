00f6309fd051e84a8da87bbdfcc35e86d3ae5291
SebaMutuku <36365043+SebaMutuku@users.noreply.github.com>
2022-10-12 23:09:15 +0300

    key addition

--- CarfuApp/utils/Security.py
+++ CarfuApp/utils/Security.py
@@ -10,6 +10,7 @@ HASH_NAME = "SHA256"
 IV_LENGTH = 16
 ITERATION_COUNT = 65536
 KEY_LENGTH = 32
+KEY = "1fUmBNzZQWEIdVpqOC+DvmlN1ShRpC7Fj9wlYoB7xSA="
 
 
 class AESEncryption:
@@ -21,10 +22,11 @@ class AESEncryption:
 
     @staticmethod
     def get_secret_key(password, salt):
+        print(hashlib.pbkdf2_hmac(HASH_NAME, password.encode(), salt.encode(), ITERATION_COUNT, KEY_LENGTH))
         return hashlib.pbkdf2_hmac(HASH_NAME, password.encode(), salt.encode(), ITERATION_COUNT, KEY_LENGTH)
 
     def encrypt(self, plain_string):
-        secret = self.get_secret_key(settings.ENC_SECRET_KEY, settings.ENC_SALT)
+        secret = base64.b64decode(settings.ENC_KEY)
         message = self.pad(plain_string)
         iv = get_random_bytes(IV_LENGTH)
         cipher = AES.new(secret, AES.MODE_CBC, iv)
@@ -32,7 +34,7 @@ class AESEncryption:
         return bytes.decode(cipher_bytes)
 
     def decrypt(self, encrypted_string):
-        secret = self.get_secret_key(settings.ENC_SECRET_KEY, settings.ENC_SALT)
+        secret = base64.b64decode(settings.ENC_KEY)
         decoded = base64.b64decode(encrypted_string)
         iv = decoded[:AES.block_size]
         cipher = AES.new(secret, AES.MODE_CBC, iv)
