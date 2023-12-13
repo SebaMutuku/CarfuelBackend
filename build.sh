affb5430c5cbc60e178dea134e4544e07ecdf14f
SebaMutuku <sebastian.muthiani@gmail.com>
2023-12-13 21:40:23 +0300

    Update migrations

--- build.sh
+++ build.sh
@@ -4,9 +4,5 @@ set -o errexit
 
 poetry install
 
-echo "***** Make Migration *****"
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
 python manage.py collectstatic --no-input
 python manage.py migrate
