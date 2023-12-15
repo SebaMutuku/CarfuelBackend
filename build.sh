c1d5ab77292337407a458d1af3bbcf252cd83833
SebaMutuku <sebastian.muthiani@gmail.com>
2023-12-15 10:40:17 +0300

    Delete migrations

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -o errexit
 poetry install
 
 
- python manage.py makemigrations
- python manage.py migrate --run-syncdb
+
+python manage.py migrate --fake admin zero
+python manage.py migrate CarfuApp zero
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
 
 python manage.py collectstatic --no-input
