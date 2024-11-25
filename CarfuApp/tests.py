8c0aa9980b590c16f93221970865d1889eae20ea
SebaMutuku <sebastian.muthiani@gmail.com>
2024-11-25 22:49:58 +0300

    Update test cases

--- CarfuApp/tests.py
+++ CarfuApp/tests.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.models import User
+
 from django.urls import reverse
 from rest_framework.test import APITestCase
+
+from CarfuApp.models import AuthUser
 
 
 class ApiTest(APITestCase):
@@ -8,9 +10,10 @@ class ApiTest(APITestCase):
 
     def test_login_returns_token(self):
         url = reverse("Login")
-        users = User.objects.get(username="", password="")
+        users = AuthUser.objects.filter(email__exact='abc@gmail.com')
+        self.assertIsNotNone(users)
         user_data = {
-            "username": "seba",
+            "username": "admin",
             "user_id": 5,
 
         }
@@ -24,7 +27,7 @@ class ApiTest(APITestCase):
             self.assertNotEqual(1, 3)
 
     def test_get_users(self):
-        users = User.objects.all()
+        users = AuthUser.objects.all()
         self.assertIsNotNone(users)
 #
 # def test_encoding(self):
